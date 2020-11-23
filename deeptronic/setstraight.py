d3a1c1c9821f556cc8d0ec3769bbc80ef081d5ac
Githubien <githubien@example.org>
2020-11-23 14:25:51 +0100

    Lines extracted

--- deeptronic/setstraight.py
+++ deeptronic/setstraight.py
@@ -15,16 +15,31 @@ class ImgProcessor:
 		self.filename=fn
 		self.debug=debug
 
-	def Process(self):
+	def process(self):
+		self.extract_lines()
+		self.fit_lines()
+		
+		
+	def extract_lines(self):
 		img0 = io.imread(self.filename)
 		img1 = color.rgb2gray(img0)
-		if (self.debug): self.Display(img0,img1)
+		#if (self.debug): self.display(img0,img1)
 		img2 = transform.rescale(img1, 0.25, anti_aliasing=True)
-		if (self.debug): self.Display(img1,img2)
+		#if (self.debug): self.display(img1,img2)
 		img3 = feature.canny(img2)
-		if (self.debug): self.Display(img2,img3)
+		#if (self.debug): self.display(img2,img3)
+
+		self.lines = transform.probabilistic_hough_line(img3, threshold=10, line_length=50,
+											  line_gap=3)
+		if (self.debug): self.display_lines(img3)
 	
-	def Display(self,imga,imgb):
+	def fit_lines(self):
+		# FIXME
+		return
+		
+		
+	
+	def display(self,imga,imgb):
 		fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 		ax = axes.ravel()
 		ax[0].imshow(imga, cmap=cm.gray)
@@ -36,12 +51,31 @@ class ImgProcessor:
 		
 		plt.tight_layout()
 		plt.show()
+		
+	def display_lines(self,img):
+		fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+		ax = axes.ravel()
+		ax[0].imshow(img, cmap=cm.gray)
+		ax[0].set_title('Step n')
+		ax[0].set_axis_off()
+		
+		ax[1].imshow(img *0)
+		for line in self.lines:
+			p0, p1 = line
+			ax[1].plot((p0[0], p1[0]), (p0[1], p1[1]))
+		ax[1].set_xlim((0, img.shape[1]))
+		ax[1].set_ylim((img.shape[0], 0))
+		ax[1].set_title('Probabilistic Hough')
+		
+		plt.tight_layout()
+		plt.show()
 
+	
 
 
 def main():
 	imp=ImgProcessor(os.path.join("import", 'img_2698.jpg'),1)
-	imp.Process()
+	imp.process()
 	
 
 
